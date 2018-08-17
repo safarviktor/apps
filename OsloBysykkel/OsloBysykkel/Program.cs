22ace20ba67cb55ae6e774cd342c0d9c8677aae8
Viktor Safar <viktor.safar@svea.com>
2018-08-17 16:09:12 +0200

    update bysykkel to run continously

--- OsloBysykkel/OsloBysykkel/Program.cs
+++ OsloBysykkel/OsloBysykkel/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Timers;
 
 
 namespace OsloBysykkel
@@ -9,7 +11,33 @@ namespace OsloBysykkel
     {
         static void Main(string[] args)
         {
-            AvailabilityLogger.Execute();
-        }        
+            var intervalInSeconds = Convert.ToInt32(ConfigurationManager.AppSettings["IntervalInSeconds"]);
+            var timer = new Timer();
+            timer.Interval = intervalInSeconds * 1000;
+            timer.Elapsed += TimerOnElapsed;
+            timer.Enabled = true;
+
+            var stopCandidate = string.Empty;
+            while (stopCandidate != "stop")
+            {
+                stopCandidate = Console.ReadLine();
+            }
+
+            timer.Enabled = false;
+        }
+
+        private static void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm")} : Checking availability . . .");
+                AvailabilityLogger.Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {ex}");
+            }
+
+        }
     }
 }
